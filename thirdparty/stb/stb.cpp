776dda320316cc30d80a3fe2d46d040196bd84a4
mike <mango@live.cn>
2019-09-20 20:55:52 +0800

    setting up

---
+++ thirdparty/stb/stb.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Mike Smith on 2019/9/20.
+//
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "repo/stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "repo/stb_image_write.h"
